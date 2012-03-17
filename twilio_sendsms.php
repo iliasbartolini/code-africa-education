908514ba8bb826a5763d9e18a7a4a1707c025839
Phil Leggetter <phil@leggetter.co.uk>
2012-03-17 17:51:36 +0000

    moving account stuff into config

--- twilio_sendsms.php
+++ twilio_sendsms.php
@@ -1,6 +1,6 @@
 <?php
 
-
+require_once('config.php');
 require_once(dirname(__FILE__) . '/includes/common.php');
 
 // no post no fun
@@ -14,8 +14,8 @@ if (empty($_POST['question'])) {
 
 require dirname(__FILE__) . '/lib/twilio/Services/Twilio.php';
 
-$account_sid = "AC8c2a05c841dd43d3b89881ea46f9d361"; // Your Twilio account sid
-$auth_token = "8f5541324b03c1a569562606bfd081a4"; // Your Twilio auth token
+$account_sid = TWILIO_ACCOUNT_SID;; // Your Twilio account sid
+$auth_token = TWILIO_AUTH_TOKEN; // Your Twilio auth token
 
 $client = new Services_Twilio($account_sid, $auth_token);
 
