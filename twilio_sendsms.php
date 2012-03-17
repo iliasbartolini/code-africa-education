e505b03eba2551123c819bb8f04e4b62c042085e
Andrei <andreiashu@gmail.com>
2012-03-17 15:50:33 +0000

    sent first msg

--- twilio_sendsms.php
+++ twilio_sendsms.php
@@ -2,13 +2,13 @@
 
 require dirname(__FILE__) . '/lib/twilio/Services/Twilio.php';
 
-$account_sid = "APb8adad61885a4874881d0f325a0535e2"; // Your Twilio account sid
+$account_sid = "AC8c2a05c841dd43d3b89881ea46f9d361"; // Your Twilio account sid
 $auth_token = "8f5541324b03c1a569562606bfd081a4"; // Your Twilio auth token
 
-$client = new Services_Twilio('AC123', '123');
+$client = new Services_Twilio($account_sid, $auth_token);
 $message = $client->account->sms_messages->create(
-  '9991231234', // From a valid Twilio number
-  '8881231234', // Text this number
+  '+442071838750', // From a valid Twilio number
+  '447951432567', // Text this number
   "Hello monkey!"
 );
 
