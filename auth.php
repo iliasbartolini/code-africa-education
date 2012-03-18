5347cdfdce21dbde53ac49570222762af8f06bd4
Andrei <andreiashu@gmail.com>
2012-03-18 16:10:55 +0000

    better auth

--- auth.php
+++ auth.php
@@ -3,7 +3,7 @@
 require_once('config.php');
 session_start();
 
-if ($_POST["username"] != USERNAME && $_POST['password'] != PASSWORD) {
+if (empty($_POST["username"]) || empty($_POST['password']) || $_POST["username"] != USERNAME || $_POST['password'] != PASSWORD) {
   header("Location: logon.php");
 }
 
