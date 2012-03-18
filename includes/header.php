14475f4c9453e4197424b65cf1682a9ab7c07388
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-03-18 16:47:04 +0000

    logo in all pages

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,6 @@
-	<header id="header">
-		<hgroup>
-			<h1 class="site_title"><a href="index.php">Dashboard</a></h1>
+	<!-- <header id="header">
+		<!-- <hgroup>
+			<h1 class="site_title">Dashboard</h1>
 			<h2 class="section_title">Simple SMS Education System</h2>
-			<div class="btn_view_site"><a href="logon.php">Logout</a></div>
-		</hgroup>
+		</hgroup> -->
 	</header> <!-- end of header bar -->
