6dd57a670e2a9abb06d3af926ce95eb22e4474ab
Ilias Bartolini <ibartoli@thoughtworks.com>
2012-03-18 16:53:34 +0000

    header is back

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
-	<!-- <header id="header">
-		<!-- <hgroup>
-			<h1 class="site_title">Dashboard</h1>
-			<h2 class="section_title">Simple SMS Education System</h2>
-		</hgroup> -->
+<header id="header">
+		<hgroup>
+			<h1 class="site_title"></h1>
+			<h2 class="section_title"></h2>
+		</hgroup>
 	</header> <!-- end of header bar -->
