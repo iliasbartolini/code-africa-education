a265b91066a6a91090208c8fcc2a2e6d01989ad7
Jianfeng Tian <jianfeng.tian@gmail.com>
2012-03-18 11:14:30 +0000

    new header to be include

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
-		<div class="row">
-			<div class="twelve columns">
-				<h1>[logo here] CodeAfrica Education</h1>
-			</div>
-		</div>
+	<header id="header">
+		<hgroup>
+			<h1 class="site_title"><a href="index.html">Simple SMS Education System</a></h1>
+			<h2 class="section_title">Dashboard</h2><div class="btn_view_site"><a href="http://www.medialoot.com">View Site</a></div>
+		</hgroup>
+	</header> <!-- end of header bar -->
